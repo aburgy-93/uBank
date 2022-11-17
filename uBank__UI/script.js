965d5ab18544bc85d79283472461f147623f8b39
aburgy-93 <aburgy@uwalumni.com>
2022-11-16 18:20:21 -0600

    Date functionality

--- uBank__UI/script.js
+++ uBank__UI/script.js
@@ -6,6 +6,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2022-10-16T17:01:17.194Z",
+    "2022-10-22T23:36:17.929Z",
+    "2022-10-23T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -13,6 +25,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -57,14 +82,32 @@ const btnClose = document.querySelector(".form__btn--close");
 const inputCloseAccount = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const btnSort = document.querySelector(".btn--sort");
+
+const labelDate = document.querySelector(".date");
+
 // Project Logic
 
+// Dates
+
 // display movements
-const displayMovements = function (movements) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
-  movements.forEach(function (movs, i) {
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
+
+  movs.forEach(function (movs, i) {
     const type = movs > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(acc.movementsDates[i]);
+
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    const displayDate = `${month}/${day}/${year}`;
 
     const html = `
       <div class="movements__row">
@@ -72,7 +115,7 @@ const displayMovements = function (movements) {
           <div class="movements__type movement__type--${type}">
             ${i + 1} ${type}
         </div>
-        <div class="movements__date">3 days ago</div>
+        <div class="movements__date">${displayDate}</div>
         <div class="movements__value">$${Math.abs(movs)}</div>
       </div>`;
 
@@ -130,7 +173,7 @@ createUsername(accounts);
 
 const updateUI = function (acc) {
   // display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
   // display balance
   calcDisplayBalance(acc);
   // display summary
@@ -150,11 +193,18 @@ btnLogin.addEventListener("click", function (e) {
   );
 
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    //     .uBank--left--nav,
-    // .account__content
     // Display UI
     accountContent.style.opacity = 100;
     leftNav.style.opacity = 100;
+
+    // Create Current date and time
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${month}/${day}/${year}, ${hour}:${min}`;
 
     // Welcome Message
     welcomeMessage.textContent = `Welcome back, ${
@@ -190,6 +240,10 @@ btnTransfer.addEventListener("click", function (e) {
     recieverAcc.movements.push(amount);
     currentAccount.movements.push(-amount);
 
+    // Add transfer date
+    currentAccount.movementsDates.push(new Date()).toISOString();
+    recieverAcc.movementsDates.push(new Date()).toISOString();
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -212,6 +266,9 @@ btnLoan.addEventListener("click", function (e) {
   ) {
     // adding the loan to account
     currentAccount.movements.push(loanAmnt);
+
+    // Add loan date
+    currentAccount.movementsDates.push(new Date()).toISOString();
 
     // Update UI
     updateUI(currentAccount);
@@ -241,3 +298,20 @@ btnClose.addEventListener("click", function (e) {
 
   inputCloseAccount.value = inputClosePin.value = "";
 });
+
+// // Sorting Arrays
+
+let sorted = false;
+
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  displayMovements(currentAccount, !sorted);
+  sorted = !sorted;
+});
+// console.log(account1.movements);
+// console.log(
+//   account1.movements.sort(function (a, b) {
+//     return a - b;
+//   })
+// );
